a66b2410716f4933ab315aeecfa521744e01a3e1
Soroush Mohammadzadeh Azari <soroush.mohammadzadeh@king.com>
2023-09-08 14:25:17 +0200

    added tris grid

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@ pub mod procedural_meshes {
             vertex.position.x = (x as f32 / resolution as f32) - 0.5;
             vertex.tex_coord0.x = x as f32 / resolution as f32;
             vertices[vi as usize] = vertex;
-            
+
             if i > 0 {
                 triangles[ti as usize] = vi - resolution - 2;
                 triangles[ti as usize + 1] = vi - resolution - 1;
@@ -64,6 +64,69 @@ pub mod procedural_meshes {
         }
     }
 
+    fn generate_tris_vertices(i: u32,
+        resolution: u32,
+        vertices: &mut std::vec::Vec<Vertex>,
+        triangles: &mut std::vec::Vec<u32>) {
+
+        let mut vi = (resolution + 1) * i;
+        let mut ti: i32 = (6 * resolution) as i32 * (i as i32 - 1);
+
+        let mut x_offset = -0.25;
+        let mut u_offset = 0.0;
+        
+        let i_a = -(resolution as i32) - 2;
+        let i_b = -(resolution as i32) - 1;
+        let i_c = -1;
+        let i_d = 0;
+        let mut t_a = IVec3::new(i_a, i_d, i_c);
+        let mut t_b = IVec3::new(i_a, i_b, i_d);
+    
+        if (i & 1) == 1 {
+            x_offset = 0.25;
+            u_offset = 0.5 / (resolution as f32 + 0.5);
+
+            t_a = IVec3::new(i_a, i_b, i_c);
+            t_b = IVec3::new(i_b, i_d, i_c);
+
+        }   
+
+        x_offset /= resolution as f32 - 0.5;
+
+        let mut vertex = Vertex::default();
+
+        vertex.normal.y = 1.0;
+        vertex.tangent.x = -1.0;
+        vertex.tangent.w = -1.0;
+
+        vertex.position.x = x_offset;
+        vertex.position.y = ((i as f32 / resolution as f32) - 0.5) * f32::sqrt(3.0) / 2.0;
+        vertex.tex_coord0.x = u_offset;
+        vertex.tex_coord0.y = vertex.position.y / (1.0 + (0.5 / resolution as f32)) + 0.5;
+
+        vertices[vi as usize] = vertex;
+
+        vi += 1;
+        for x in 1..resolution + 1 {
+            vertex.position.x = (x as f32 / resolution as f32) + x_offset;
+            vertex.tex_coord0.x = x as f32 / (resolution as f32 + 0.5) + u_offset;
+            vertices[vi as usize] = vertex;
+
+            if i > 0 {
+                triangles[ti as usize] = (vi as i32 + t_a.x) as u32;
+                triangles[ti as usize + 1] = (vi as i32 + t_a.y) as u32;
+                triangles[ti as usize + 2] = (vi as i32 + t_a.z) as u32;
+                triangles[ti as usize + 3] = (vi as i32 + t_b.x) as u32;
+                triangles[ti as usize + 4] = (vi as i32 + t_b.y) as u32;
+                triangles[ti as usize + 5] = (vi as i32 + t_b.z) as u32;
+            }
+
+            vi += 1;
+            ti += 6;
+        }
+
+    }
+    
     pub fn generate_grid(resolution: u32) -> Mesh {
         let vertex_count: u32 = (resolution + 1) * (resolution + 1);
         let index_count: u32 = 6 * resolution * resolution;
@@ -72,9 +135,8 @@ pub mod procedural_meshes {
         let mut triangles = vec![0 as u32; index_count as usize];
 
         for i in 0..resolution + 1 {
-            generate_quad_vertices(i, resolution, &mut vertices, &mut triangles);
+            generate_tris_vertices(i, resolution, &mut vertices, &mut triangles);
         }
-
 
         let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
         mesh.insert_attribute(
@@ -101,4 +163,5 @@ pub mod procedural_meshes {
 
         mesh
     }
+
 }
